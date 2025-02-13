7a66b38f2f8c9d556bb5d96517cb58bd3c28a4e7
Rahul kumar <stkwhy000@gmail.com>
2025-02-14 02:08:15 +0530

    push

--- src/app/page.js
+++ src/app/page.js
@@ -14,9 +14,9 @@ const Page = () => {
    <div className=' w-full h-full bg-black overflow-hidden'>
      <div className=' flex items-center justify-center max-w-screen-xl flex-col space-y-6 mx-auto h-screen'>
       <div className=' max-w-screen-xl mx-auto flex items-center flex-col justify-center relative z-20'>
-        <h1 className='lg:text-[130px]  font-bold text-transparent bg-clip-text bg-gradient-to-r font-serif from-purple-500 to-purple-400 z-20'>LoveTunes</h1>
-        <p className='lg:text-2xl text-sm font-bold text-white text-center'>Where Love and Music Sync</p>
-        <Image src="/heart.png" alt="music" width={100} height={100} className='absolute -top-8 -right-20 animate-pulse' />
+        <h1 className='md:text-8xl text-3xl  font-bold text-transparent bg-clip-text bg-gradient-to-r font-serif from-purple-500 to-purple-400 z-20'>Chill Zone</h1>
+        <p className='lg:text-2xl text-xl font-bold text-white text-center'>music taste matter</p>
+        <Image src="/hugy.png" alt="music" width={100} height={100} className='absolute -top-8 -right-20 animate-pulse' />
         <div className='absolute -bottom-8 -left-20  bg-gradient-to-r from-orange-100 to-yellow-100 w-[700px] 
         h-[700px] rounded-full blur-3xl opacity-30 '></div>
         <div className='absolute -top-8 -right-40  bg-gradient-to-r from-pink-100 to-red-100 w-[500px] 
