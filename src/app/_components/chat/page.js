999b5838288accfccff8c1e3303b0e83c92e6ad0
Rahul kumar <stkwhy000@gmail.com>
2025-02-14 01:50:05 +0530

    done

--- src/app/_components/chat/page.js
+++ src/app/_components/chat/page.js
@@ -5,8 +5,8 @@ import { io } from "socket.io-client";
 import { v4 as uuidv4 } from "uuid";
 
 const ChatRoom = ({ roomId }) => {
-    //   const socket = useMemo(() => io("https://lovetunes-2.onrender.com"), []);
-    const socket = useMemo(() => io("http://localhost:3001"), []);
+      const socket = useMemo(() => io("https://room-music-player-server.onrender.com/"), []);
+    // const socket = useMemo(() => io("http://localhost:3001"), []);
     const [message, setMessage] = useState("");
     const [chat, setChat] = useState([]);
     const [userId, setUserId] = useState("");
